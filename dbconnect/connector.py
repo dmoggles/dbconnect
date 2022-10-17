8b36a0812a97de456f6fcd29ecdaf8bfd459e5f3
Dmitry Mogilevsky <dmitry.mogilevsky@gmail.com>
2022-10-18 00:44:54 +0100

    switching mysql engine

--- dbconnect/connector.py
+++ dbconnect/connector.py
@@ -11,7 +11,7 @@ DATABASE = "football_data"
 
 def mysql_engine(password, user=USER, host=HOST, port="3306", database=DATABASE):
     engine = create_engine(
-        "mysql://{0}:{1}@{2}:{3}/{4}?charset=utf8".format(
+        "mysql+mysqlconnector://{0}:{1}@{2}:{3}/{4}?charset=utf8".format(
             user, password, host, port, database
         )
     )
